078d01500761187428e0d7191b578c28357cdf8f
Ron Nielsen <ronaldnielsen@gmail.com>
2020-06-29 11:35:49 -0700

    styled-components samples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import styled from 'styled-components';
+
+const Header = styled.div`
+  padding: 16px;
+  background: red;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+const Button = styled.button`
+  padding: 8px 16px;
+  background: blue;
+  color: white;
+  font-size: 14px;
+  border: none;
+  outline: none;
+  border-radius: 8px;
+  margin: 16px;
+  &:active {
+    background: darkblue;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      Home
+      <Header>
+        I am the header
+      </Header>
+      <Button>
+        Press Me
+      </Button>
     </div>
   );
 }

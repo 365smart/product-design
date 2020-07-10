51ab14ffcc82d60a7e9d0f61a405b25113be3369
Ron Nielsen <ronaldnielsen@gmail.com>
2020-07-10 11:47:00 -0700

    merges

--- src/components/molecules/stories/Footermessage.stories.js
+++ src/components/molecules/stories/Footermessage.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Footermessage } from '../';
 
-export default { title: 'molecules.Button' };
+export default { title: 'molecules.Footermessage' };
 
 export const plain = () => <Footermessage message="Scan an item at any time" />;
